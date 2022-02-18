4f9237c655dc78861614f3b916e2f501067ef8b9
tomthomas1 <tomthomas8688@gmail.com>
2022-02-18 20:30:19 +0530

    Reaches winning position 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -7,30 +7,38 @@ public class SnakeAndLadder {
 		int player_pos = 0;
 		final int ladder = 1;
 		final int snake = 2;
+		int option;
+		int totalmoves=0;
 
 		System.out.println("Player starting position is: " + player_pos);
 
-		int dice = (int) Math.floor((Math.random() * 7));
-		System.out.println("Dice Value is : " + dice);
+		while (player_pos < 100) {
+			int dice = (int) Math.floor((Math.random() * 7));
+			System.out.println("Dice Value is : " + dice);
 
-		int option = (int) Math.floor((Math.random() * 3));
+			option = (int) Math.floor((Math.random() * 3));
 
-		switch (option) {
+			switch (option) {
 
-		case ladder:
-			System.out.println(" Current position has ladder");
-			player_pos += dice;
-			break;
-		case snake:
-			System.out.println(" Current position had snake");
-			if(player_pos < 0) {
-			player_pos -= dice;
+			case ladder:
+				System.out.println(" Current position has ladder");
+				player_pos = player_pos + dice;
+				totalmoves++;
+				break;
+			case snake:
+				System.out.println(" Current position had snake");
+				player_pos = player_pos - dice;
+				player_pos = player_pos < 0 ? 0 : player_pos;
+				totalmoves++;
+				break;
+			default:
+				System.out.println(" No play.");
+				totalmoves++;
 			}
-			break;
-		default:
-			System.out.println(" No play.");
+			System.out.println(" Current position is: " + player_pos);
 		}
-		System.out.println(" Current position is: " + player_pos);
+		System.out.println("Total moves taken to win the game is : " + totalmoves);
+		System.out.println(" Congratulations. You have won the game..");
 	}
 
 }
