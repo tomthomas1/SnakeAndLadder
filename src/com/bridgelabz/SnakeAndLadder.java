bf2e5094600a244a2ecd311a24eb7d52ab29eea5
tomthomas1 <tomthomas8688@gmail.com>
2022-02-18 20:12:13 +0530

    Rolls the die

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,8 +5,10 @@ public class SnakeAndLadder {
 		System.out.println(" Welcome to the Snake And Ladder Game");
 
 		int player_pos = 0;
-
 		System.out.println("Player position is: " + player_pos);
+
+		int dice = (int) Math.floor((Math.random() * 7));
+		System.out.println("Position after dice rolled is : " + dice);
 
 	}
 
