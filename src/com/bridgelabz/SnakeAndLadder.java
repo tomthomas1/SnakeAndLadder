54e81c31c2e4c8e28159a5c3be4ded95de968ff2
tomthomas1 <tomthomas8688@gmail.com>
2022-02-18 20:44:43 +0530

    Win but not exceed 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,29 +9,37 @@ public class SnakeAndLadder {
 		final int snake = 2;
 		int option;
 		int totalmoves=0;
-
+          //initalize the starting position.
 		System.out.println("Player starting position is: " + player_pos);
 
 		while (player_pos < 100) {
-			int dice = (int) Math.floor((Math.random() * 7));
+			int dice = (int) Math.floor((Math.random() * 7));   // random function for dice
 			System.out.println("Dice Value is : " + dice);
 
-			option = (int) Math.floor((Math.random() * 3));
+			option = (int) Math.floor((Math.random() * 3)); // random function for moves
 
 			switch (option) {
-
+                // if random function is 1 then it is ladder and will increment the position.
 			case ladder:
-				System.out.println(" Current position has ladder");
+				System.out.println(" Current position has ladder H");
+				System.out.println("                             H");
 				player_pos = player_pos + dice;
+				//if the position is greater that 100 then it will not take any move.
+				if (player_pos > 100) {
+					player_pos = player_pos - dice;
+					System.out.println(" NOT VALID. Please continue to play as current position greater that 100.");
+				}
 				totalmoves++;
 				break;
 			case snake:
-				System.out.println(" Current position had snake");
+				// if random function is 2 then it is snake and will decrement the position.
+				System.out.println(" Current position had snake ~~~~~~~~>>");
 				player_pos = player_pos - dice;
 				player_pos = player_pos < 0 ? 0 : player_pos;
 				totalmoves++;
 				break;
 			default:
+				// else there will be no moves and will be in the current position.
 				System.out.println(" No play.");
 				totalmoves++;
 			}
